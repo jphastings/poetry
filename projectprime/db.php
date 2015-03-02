cb9189f7c293814177be21846c0eceae342a033e
JP Hastings-Spital <jphastings@gmail.com>
2015-03-02 13:02:52 +0000

    Remove database access

--- projectprime/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (!@mysql_connect("kedakai.co.uk","kedakaic_pprime","3tgvD@@k2%NX")) {
-	die("Technical Difficulties! Please come back soon.");
-}
-if (!@mysql_select_db("kedakaic_miniProjects")) {
-	die("Can't connect to database");
-}
